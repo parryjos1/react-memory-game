8f318656f6dd5764be99c31f8a67891aba6d9316
Josh Parry <josh.parry97@gmail.com>
2020-02-15 15:57:33 +1100

    Updates

---
+++ precache-manifest.5aba36b90add245c94623e78c3cb723c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1bb6343bad070529f9d3ded64d3998",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "5231aac6e7e220afc8ef",
+    "url": "/react-memory-game/static/css/main.6584da29.chunk.css"
+  },
+  {
+    "revision": "6f490433cb71a68521cb",
+    "url": "/react-memory-game/static/js/2.7874a470.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-memory-game/static/js/2.7874a470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5231aac6e7e220afc8ef",
+    "url": "/react-memory-game/static/js/main.7483e8be.chunk.js"
+  },
+  {
+    "revision": "08cb7aa86876e137cf93",
+    "url": "/react-memory-game/static/js/runtime-main.2325090e.js"
+  },
+  {
+    "revision": "63538a6589922c1cb9cce15c407fe342",
+    "url": "/react-memory-game/static/media/poker.63538a65.svg"
+  }
+]);
